4fa7c11b31c0f2fcb9b8e87f0288b8add4f77b6d
Peter Solymos <psolymos@gmail.com>
2019-09-11 13:53:37 -0600

    use GH_TOKEN

--- _deploy.sh
+++ _deploy.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-set -e
+set -o errexit -o nounset
 
-[ -z "${GITHUB_PAT}" ] && exit 0
-echo PAT OK
+[ -z "${GH_TOKEN}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
-echo branch OK
 
 git config --global user.email "psolymos@gmail.com"
 git config --global user.name "Peter Solymos"
-echo git config OK
 
-git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
-echo clone OK
+git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
 cp -r ../_book/* ./
 git add --all *
-echo copy OK
-git commit -m "Update the book" || true
-echo commit OK
+git commit -m "Update the book (${TRAVIS_BUILD_NUMBER})" || true
 git push -q origin gh-pages
-echo push OK
